e6208dac4fbbc783825adf00bc9ee66233689629
tesfahailu <40552713+tesfahailu@users.noreply.github.com>
2020-05-31 11:35:27 -0400

    feat: install prettier package, dotenv package and implement usage in angular

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -18,7 +18,7 @@ describe('workspace-project App', () => {
     expect(logs).not.toContain(
       jasmine.objectContaining({
         level: logging.Level.SEVERE,
-      } as logging.Entry)
+      } as logging.Entry),
     );
   });
 });
