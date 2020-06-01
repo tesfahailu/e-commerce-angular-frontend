d429af61bf5b56c54c7dff83c8ed3e81bb93aeca
Tesfa Hailu <>
2020-06-01 16:50:32 -0400

    fix: added environment variables to hide sensitive environment data

--- set-env.js
+++ set-env.js
@@ -1,20 +1,40 @@
-require("dotenv").config();
-const fs = require("fs");
-const colors = require("colors");
-const targetPath = "./src/environments/environment.ts";
-const targetProdPath = "./src/environments/environment.prod.ts";
+require('dotenv').config();
+const fs = require('fs');
+const colors = require('colors');
+const targetPath = './src/environments/environment.ts';
+const targetProdPath = './src/environments/environment.prod.ts';
 const envConfigFile = `export const environment = {
   production: false,
+    firebase: {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+  }
 };`;
 
 const envConfigProdFile = `export const environment = {
   production: true,
+    firebase: {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+  }
 }`;
 
 console.log(
   colors.magenta(
-    "The file `environment.ts` will be written with the following content: \n"
-  )
+    'The file `environment.ts` will be written with the following content: \n',
+  ),
 );
 console.log(colors.grey(envConfigFile));
 fs.writeFile(targetPath, envConfigFile, function (err) {
@@ -23,15 +43,15 @@ fs.writeFile(targetPath, envConfigFile, function (err) {
   } else {
     console.log(
       colors.magenta(
-        `Angular environment.ts file generated correctly at ${targetPath} \n`
-      )
+        `Angular environment.ts file generated correctly at ${targetPath} \n`,
+      ),
     );
   }
 });
 console.log(
   colors.magenta(
-    "The file `environment.prod.ts` will be written with the following content: \n"
-  )
+    'The file `environment.prod.ts` will be written with the following content: \n',
+  ),
 );
 console.log(colors.grey(envConfigProdFile));
 fs.writeFile(targetProdPath, envConfigProdFile, function (err) {
@@ -40,8 +60,8 @@ fs.writeFile(targetProdPath, envConfigProdFile, function (err) {
   } else {
     console.log(
       colors.magenta(
-        `Angular environment.prod.ts file generated correctly at ${targetProdPath} \n`
-      )
+        `Angular environment.prod.ts file generated correctly at ${targetProdPath} \n`,
+      ),
     );
   }
 });
