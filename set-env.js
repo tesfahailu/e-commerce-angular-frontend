3aa72e918936153cebe5f813b42d892177b7de8d
Tesfa Hailu <>
2020-06-01 20:21:48 -0400

    fix: error in interpolating firebase config string

--- set-env.js
+++ set-env.js
@@ -6,28 +6,28 @@ const targetProdPath = './src/environments/environment.prod.ts';
 const envConfigFile = `export const environment = {
   production: false,
     firebase: {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+    apiKey: '${process.env.API_KEY}',
+    authDomain: '${process.env.AUTH_DOMAIN}',
+    databaseURL: '${process.env.DATABASE_URL}',
+    projectId: '${process.env.PROJECT_ID}',
+    storageBucket: '${process.env.STORAGE_BUCKET}',
+    messagingSenderId: '${process.env.MESSAGING_SENDER_ID}',
+    appId: '${process.env.APP_ID}',
+    measurementId: '${process.env.MEASUREMENT_ID}'
   }
 };`;
 
 const envConfigProdFile = `export const environment = {
   production: true,
     firebase: {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+    apiKey: '${process.env.API_KEY}',
+    authDomain: '${process.env.AUTH_DOMAIN}',
+    databaseURL: '${process.env.DATABASE_URL}',
+    projectId: '${process.env.PROJECT_ID}',
+    storageBucket: '${process.env.STORAGE_BUCKET}',
+    messagingSenderId: '${process.env.MESSAGING_SENDER_ID}',
+    appId: '${process.env.APP_ID}',
+    measurementId: '${process.env.MEASUREMENT_ID}'
   }
 }`;
 
