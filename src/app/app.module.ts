c25d37767904d03988019e549f923ec15d3a86d4
Tesfa Hailu <>
2020-06-02 08:30:54 -0400

    feat: added navbar to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BsNavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
