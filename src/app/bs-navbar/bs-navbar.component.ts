7f4149cdb197dc5365a74c1f470ada21eb37ba38
tesfahailu <40552713+tesfahailu@users.noreply.github.com>
2020-06-06 15:16:05 -0400

    feat: add routing to project and component pages to test routing (#8)

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,11 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
+  styleUrls: ['./bs-navbar.component.scss'],
 })
-export class BsNavbarComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class BsNavbarComponent {}
