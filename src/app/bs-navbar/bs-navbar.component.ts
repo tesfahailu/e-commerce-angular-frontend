191272daad4d93a9200bb3f37b9b52a098c88a07
tesfahailu <40552713+tesfahailu@users.noreply.github.com>
2020-06-06 14:33:24 -0400

    feat: add navbar to project

---
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-navbar',
+  templateUrl: './bs-navbar.component.html',
+})
+export class BsNavbarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
