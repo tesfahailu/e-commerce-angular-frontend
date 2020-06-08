b2f2d40dfbf53d7266bf4f909078ca4a3cd0e521
tesfahailu <40552713+tesfahailu@users.noreply.github.com>
2020-06-08 09:39:23 -0400

    feat: add auth service and save user to firebase db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
+import { UserService } from './user.service';
+import { AuthService } from './auth.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'shop';
+  constructor(
+    private userService: UserService,
+    private auth: AuthService,
+    router: Router,
+  ) {
+    auth.user$.subscribe((user) => {
+      if (user) {
+        userService.save(user);
+      }
+    });
+  }
 }
