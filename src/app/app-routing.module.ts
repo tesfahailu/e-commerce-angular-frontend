846b70462b75104a1642e54da3fdb14a2b0eab7e
Tesfa Hailu <>
2020-06-11 18:58:15 -0400

    feat: edit a product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,15 +26,19 @@ const routes: Routes = [
     canActivate: [AuthGuard],
   },
   { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
-
-  {
-    path: 'admin/products',
-    component: AdminProductsComponent,
-    canActivate: [AuthGuard, AdminAuthGuard],
-  },
   {
     path: 'admin/products/new',
     component: ProductFormComponent,
+    canActivate: [AuthGuard, AdminAuthGuard],
+  },
+  {
+    path: 'admin/products/:id',
+    component: ProductFormComponent,
+    canActivate: [AuthGuard, AdminAuthGuard],
+  },
+  {
+    path: 'admin/products',
+    component: AdminProductsComponent,
     canActivate: [AuthGuard, AdminAuthGuard],
   },
   {
