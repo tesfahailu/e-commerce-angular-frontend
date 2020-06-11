31c3b2fb04cf16db083bbdf29c386d191b991880
Tesfa Hailu <>
2020-06-11 10:23:01 -0400

    feat: display products in list

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,3 +1,4 @@
+import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 
@@ -9,5 +10,20 @@ export class ProductService {
 
   create(product) {
     return this.db.list('/products').push(product);
+  }
+
+  getAll() {
+    return this.db
+      .list('/products')
+      .snapshotChanges()
+      .pipe(
+        map((action) =>
+          action.map((a) => {
+            const key = a.payload.key;
+            const data = a.payload.val();
+            return { key, data };
+          }),
+        ),
+      );
   }
 }
