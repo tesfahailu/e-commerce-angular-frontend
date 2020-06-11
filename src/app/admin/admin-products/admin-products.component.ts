5929bf901868bf1eee871c8b472eade9d3a336be
Tesfa Hailu <>
2020-06-11 10:23:01 -0400

    feat: display products in list

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { async } from '@angular/core/testing';
+import { ProductService } from './../../product.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
 })
-export class AdminProductsComponent {}
+export class AdminProductsComponent implements OnInit {
+  products$;
+
+  constructor(private productService: ProductService) {
+    this.products$ = this.productService.getAll();
+  }
+
+  ngOnInit() {}
+}
