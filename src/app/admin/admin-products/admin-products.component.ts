6315c63604b0c73e53e3977cfaecf7267e9d70c1
Tesfa Hailu <>
2020-06-13 15:23:52 -0400

    feat: create a search box and filter functionality

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,16 +1,34 @@
-import { async } from '@angular/core/testing';
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: any[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService
+      .getAll()
+      .subscribe(
+        (products) => (this.filteredProducts = this.products = products),
+      );
+  }
+
+  filter(query: string) {
+    this.filteredProducts = query
+      ? this.products.filter(({ data: { title } }) =>
+          title.toLowerCase().includes(query.toLowerCase()),
+        )
+      : this.products;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   ngOnInit() {}
