b1e73bbb2d46470b67e550a11c2045f52ce4cc9a
Tesfa Hailu <>
2020-06-17 22:59:00 -0400

    feat: implement simple category list on homepage

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,13 +1,23 @@
+import { Observable } from 'rxjs';
+import { CategoryService } from './../category.service';
 import { ProductService } from './../product.service';
 import { Component } from '@angular/core';
+import { Product } from '../models/product';
+import { Category } from '../models/category';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
 })
 export class ProductsComponent {
-  productsObservable;
-  constructor(productService: ProductService) {
+  productsObservable: Observable<Product[]>;
+  categoriesObservable: Observable<Category[]>;
+
+  constructor(
+    productService: ProductService,
+    categoryService: CategoryService,
+  ) {
     this.productsObservable = productService.getProductsObservable();
+    this.categoriesObservable = categoryService.getCategoriesObservable();
   }
 }
