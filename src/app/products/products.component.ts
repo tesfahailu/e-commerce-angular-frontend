1866517f92f7fba0e571d53577c13e1ac5b2d239
Tesfa Hailu <>
2020-06-18 11:27:17 -0400

    feat: apply filter to products on homepage by product category

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,23 +1,35 @@
+import { Category } from './../models/category';
 import { Observable } from 'rxjs';
 import { CategoryService } from './../category.service';
 import { ProductService } from './../product.service';
 import { Component } from '@angular/core';
 import { Product } from '../models/product';
-import { Category } from '../models/category';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
 })
 export class ProductsComponent {
-  productsObservable: Observable<Product[]>;
+  products: Product[];
+  filteredProducts: Product[];
   categoriesObservable: Observable<Category[]>;
+  paramCategory: string;
 
   constructor(
+    route: ActivatedRoute,
     productService: ProductService,
     categoryService: CategoryService,
   ) {
-    this.productsObservable = productService.getProductsObservable();
+    productService
+      .getProductsObservable()
+      .subscribe((products) => (this.products = products));
     this.categoriesObservable = categoryService.getCategoriesObservable();
+    route.queryParamMap.subscribe((params) => {
+      this.paramCategory = params.get('category');
+      this.filteredProducts = this.paramCategory
+        ? this.products.filter((p) => p.category === this.paramCategory)
+        : this.products;
+    });
   }
 }
