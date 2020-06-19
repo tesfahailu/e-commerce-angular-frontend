b692d86140a87f6fc42b919e1dd98cfa98a55e7f
Tesfa Hailu <>
2020-06-19 08:54:29 -0400

    fix: product card styling of add button

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -4,6 +4,7 @@ import { Product } from '../models/product';
 @Component({
   selector: 'product-card',
   templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent {
   @Input('product') product: Product;
