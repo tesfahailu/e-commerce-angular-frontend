03be6e388a972e6dff7788f695f78f9976199d9e
Tesfa Hailu <>
2020-06-23 14:15:02 -0400

    fix: remove unused CSS but didn't update the components metadata to reflect change

--- src/app/shopping-cart-summary/shopping-cart-summary.component.ts
+++ src/app/shopping-cart-summary/shopping-cart-summary.component.ts
@@ -4,7 +4,6 @@ import { ShoppingCart } from '../models/shopping-cart';
 @Component({
   selector: 'shopping-cart-summary',
   templateUrl: './shopping-cart-summary.component.html',
-  styleUrls: ['./shopping-cart-summary.component.css'],
 })
 export class ShoppingCartSummaryComponent {
   @Input('cart') cart: ShoppingCart;
